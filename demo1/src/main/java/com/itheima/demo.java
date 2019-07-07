22ca9e57154bd06e68bca75ded31a9111f8b9791
sherlock <798522378@qq.com>
2019-07-07 10:37:12 +0800

    第一次提交

---
+++ demo1/src/main/java/com/itheima/demo.java
@@ -0,0 +1,9 @@
+package com.itheima;
+
+public class demo {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("haha"+i);
+        }
+    }
+}
